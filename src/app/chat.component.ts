0083cb454a983cc7ba57db39789d7dacfdc6596d
lq150415 <lq150415@gmail.com>
2018-04-12 03:49:13 -0400

    First commit

---
+++ src/app/chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Darsalud-chat',
+  templateUrl: './chat.component.html',
+  styles: []
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
